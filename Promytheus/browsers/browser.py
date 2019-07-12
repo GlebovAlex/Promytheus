55c356075d2694544790d695fe52107361a32add
Joan Lee <jolee9123@gmail.com>
2019-07-11 23:45:41 -0700

    Add .gitignore

--- Promytheus/browsers/browser.py
+++
@@ -1,6 +0,0 @@
-class Browser:
-
-    def __init__(self):
-
-        self.chrome = "C:/Users/jolee/Desktop/Selenium/Promytheus/drivers/chromedriver.exe"
-        self.firefox = "C:/Users/jolee/Desktop/Selenium/Promytheus/drivers/chromedriver.exe"
