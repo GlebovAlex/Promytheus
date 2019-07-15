40ca0434e586ac411ac38d9a8b0d37d228c3a524
Joan Lee <jolee9123@gmail.com>
2019-07-15 10:25:25 -0700

    Update changes

--- Promytheus/main.py
+++ Promytheus/main.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 import subprocess
-subprocess.call("./tests/test_login.py")
+
+subprocess.call("./tests/test_login_page.py")
+subprocess.call("./tests/test_password_change.py")
+subprocess.call("./tests/test_talent_form_evidence_tab.py")
+subprocess.call("./tests/test_talent_form_training.py")
