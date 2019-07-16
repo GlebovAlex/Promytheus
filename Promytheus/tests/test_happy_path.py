81046deaf118fe2adcfa692b23f2f9e6bc1e548e
Joan Lee <jolee9123@gmail.com>
2019-07-16 00:38:53 -0700

    Update test_password_change.py

--- Promytheus/tests/test_happy_path.py
+++ Promytheus/tests/test_happy_path.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/python3
 from selenium import webdriver
 import unittest
 import time
--- Promytheus/tests/test_password_change.py
+++ Promytheus/tests/test_password_change.py
@@ -1,3 +1,4 @@
+#!usr/bin/python3
 from selenium import webdriver
 import time
 import json
