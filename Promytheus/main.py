25b053c3af81b6816fca4655af796d75398ac6e1
Joan Lee <jolee9123@gmail.com>
2019-07-15 22:46:54 -0700

    Update main.py

--- Promytheus/main.py
+++ Promytheus/main.py
@@ -1,7 +1,4 @@
-#!usr/bin/python3
+#! /usr/bin/env python3
 import subprocess
 
 subprocess.call("./tests/test_login_page.py")
-subprocess.call("./tests/test_password_change.py")
-subprocess.call("./tests/test_talent_form_evidence_tab.py")
-subprocess.call("./tests/test_talent_form_training.py")
