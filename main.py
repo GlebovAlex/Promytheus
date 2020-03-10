ec2d689c4597984e4d7689dfe83762a77956beaa
Alex Glebov <sashochek66@gmail.com>
2020-03-09 18:48:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 import subprocess
 
 subprocess.call("./Promytheus/tests/test_login_page.py")
-subprocess.call("./Promytheus/tests/test_personalitytraits_page.py")
+#subprocess.call("./Promytheus/tests/test_personalitytraits_page.py")
